56d8a956991fc9bbda63a7ad538aedcc46fd1a2a
DariaNovakoova <novakova2003dasha@gmail.com>
2023-11-21 19:24:45 +0200

    3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -13,11 +13,46 @@ HTML містить розмітку форми, в поля якої корис
 Для відображення повідомлень користувачеві, замість console.log(), використовуй бібліотеку notiflix.
 */
 
+import Notiflix from 'notiflix';
+
+const promiseForm = document.querySelector('.form');
+
+let delay = 0;
+
+promiseForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const amount = Number(form.elements.amount.value);
+  const step = Number(form.elements.step.value);
+  delay = Number(form.elements.delay.value);
+
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delay += step;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
